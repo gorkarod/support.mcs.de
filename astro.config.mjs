5cd190ed144ed3d7d1475bfbef6fda7b1ed1000a
Hartmut Junker <mail@hartmutjunker.de>
2025-04-15 06:26:08 +0200

    Fonts integriert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [
+            Unfonts({
+                custom: {
+                    display: 'swap',
+                    families: {
+                        'TStar': {
+                            src: './assets/fonts/tstar*',
+                            transform(font) {
+                                if (font.basename === 'tstar-bold')
+                                    font.weight = 700
+                                return font
+                            }
+                        }
+                    }
+                }
+            })
+        ]
+    }
+});
